d2cb8eae9ab3a1bb0019afdcfc5a5af092464d50
wilsjo2 <97138003+wilsjo2@users.noreply.github.com>
2026-09-13 12:35:40 +1000

    Make RTX 40 MFG unlock an opt-in build feature

--- OptiScaler/menu/menu_common.cpp
+++ OptiScaler/menu/menu_common.cpp
@@ -1,7 +1,9 @@
 ﻿#include "pch.h"
 #include <dlssnr/DlssNr_MenuOverlay.h>
 #include "menu_common.h"
+#if defined(OPTISCALER_RTX40_MFG)
 #include <framegen/dlssg/MfgUnlock.h>
+#endif
 #include <dlssnr/DlssNr_ExposureScan.h>
 
 #include <algorithm>
@@ -3153,6 +3155,7 @@ void MenuCommon::RenderFrameGenerationSelection(RenderMenuContext& ctx)
     auto& menuResScale = ctx.menuResScale;
     auto& primaryGpu = *ctx.primaryGpu;
 
+#if defined(OPTISCALER_RTX40_MFG)
     const bool adaEnabledForSession = MfgUnlock::EnabledForSession();
     bool adaUnlock = config->FGDLSSGAdaMfgUnlock.value_or_default();
     const bool isAda = primaryGpu.vendorId == VendorId::Nvidia &&
@@ -3175,6 +3178,7 @@ void MenuCommon::RenderFrameGenerationSelection(RenderMenuContext& ctx)
         else
             ImGui::TextWrapped("DLSSG %s: unlock unavailable for this runtime.", status.SnippetVersion.c_str());
     }
+#endif
 
     /// FG INPUTS
     static std::vector<MenuOption<FGInput>> inputOptions;
